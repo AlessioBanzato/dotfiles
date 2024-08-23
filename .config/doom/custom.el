39e5398f74e45c1fc6f8fb61a8a67713455b9c7e
AlessioBanzato <alessiobanzato@gmail.com>
2024-08-23 13:30:52 +0200

    emacs - added org roam package

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ispell-dictionary nil)
- '(package-selected-packages '(doom-themes org-bullets cmake-mode)))
+ '(package-selected-packages '(org-roam doom-themes org-bullets cmake-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
