fea867d721c50cad9ed05c4e1fe1866673bf423b
AlessioBanzato <alessiobanzato@gmail.com>
2025-02-09 14:54:54 +0100

    emacs org-mode - keybindings

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -120,17 +120,18 @@
 
 ;; org-roam
 (use-package org-roam
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
   :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode))
+
+(map! :leader
+      :prefix "n"
+      :desc "org-roam" "l" #'org-roam-buffer-toggle
+      :desc "org-roam-node-insert" "i" #'org-roam-node-insert
+      :desc "org-roam-node-find" "f" #'org-roam-node-find
+      :desc "org-roam-show-graph" "g" #'org-roam-show-graph
+      :desc "org-roam-capture" "c" #'org-roam-capture
+      :desc "org-roam-dailies-capture-today" "j" #'org-roam-dailies-capture-today)
 
 ;; org-roam-ui
 (use-package! websocket
