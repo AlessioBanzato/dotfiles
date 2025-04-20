844adaf69489cfde591127cb43e029a8eba235ae
AlessioBanzato <alessiobanzato@gmail.com>
2025-04-20 12:08:33 +0200

    emacs conf

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-roam-ui org-bullets org-roam sqlite3 pdf-tools magit-section emacsql dash)))
+   '(auctex auctex-latexmk auto-complete-auctex dash emacsql magit-section
+     org-bullets org-roam org-roam-ui pdf-tools sqlite3)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
